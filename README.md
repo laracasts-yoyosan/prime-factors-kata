78bbe86f0d478bc4527900433237de5b340cd2af
Marius Palade <marius.palade@blablacar.com>
2016-04-17 18:26:52 +0300

    Prime factors kata.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Problem
+
+Write a suite of tests that display the prime factors for a number.
+
+# Examples
+
+The prime factor/s for:
+* 4 are 2 * 2 or [2, 2]
+* 5 is [5]
+* 6 are 2 * 3 or [2, 3]
+* 12 are 2 * 2 * 3 or [2, 2, 3]
+* 50 are 2 * 5 * 5 or [2, 2, 5]
+
+# Usage
+
+```
+composer install
+./vendor/bin/phpspec run
+```
---
+++ src/PrimeFactors.php
@@ -0,0 +1,22 @@
+<?php
+
+class PrimeFactors
+{
+    /**
+     * Generate prime factors.
+     * @param  int $number
+     * @return array
+     */
+    public function generate($number)
+    {
+        $primes = [];
+
+        for ($candidate = 2; $number > 1; $candidate++) {
+            for (; $number % $candidate == 0; $number /= $candidate) {
+                $primes[] = $candidate;
+            }
+        }
+
+        return $primes;
+    }
+}
